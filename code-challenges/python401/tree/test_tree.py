d6cadffed8524a103e3e990acf38ac26ec366183
EvyHaan <evy.haan@gmail.com>
2019-03-09 11:35:00 -0800

    binary tree pre- post- in- order functions running

--- code-challenges/python401/tree/test_tree.py
+++ code-challenges/python401/tree/test_tree.py
@@ -1,5 +1,6 @@
 from tree import Node, BinaryTree, BinarySearchTree
 
+# ====== Binary Search Tree tests ======
 
 def test_BST_class_exists():
     assert BinarySearchTree
@@ -66,7 +67,7 @@ def test_add_five_nodes():
     assert bst.root.right_child.left_child.value == 15
 
 
-def test_add_five_nodes():
+def test_search_one_node_truthy():
     bst = BinarySearchTree()
     bst.add(10)
     bst.add(5)
@@ -74,8 +75,108 @@ def test_add_five_nodes():
     bst.add(8)
     bst.add(15)
 
-    assert bst.root.value == 10
-    assert bst.root.left_child.value == 5
-    assert bst.root.right_child.value == 20
-    assert bst.root.left_child.right_child.value == 8
-    assert bst.root.right_child.left_child.value == 15
+    expected = True
+    actual = bst._contains(8)
+
+    assert actual == expected
+
+
+def test_search_another_node_truthy():
+    bst = BinarySearchTree()
+    bst.add(10)
+    bst.add(5)
+    bst.add(20)
+    bst.add(8)
+    bst.add(15)
+
+    expected = True
+    actual = bst._contains(5)
+
+    assert actual == expected
+
+
+def test_search_falsey():
+    bst = BinarySearchTree()
+    bst.add(10)
+    bst.add(5)
+    bst.add(20)
+    bst.add(8)
+    bst.add(15)
+
+    expected = False
+    actual = bst._contains(25)
+
+    assert actual == expected
+
+
+def test_search_falsey():
+    bst = BinarySearchTree()
+
+    expected = False
+    actual = bst._contains(10)
+
+    assert actual == expected
+
+# ====== Binary Tree Tests =======
+
+
+def test_BTree_class_exists():
+    assert BinaryTree
+
+
+def test_BTree_instantiation():
+    assert BinaryTree()
+
+
+def test_preorder():
+    tree = BinarySearchTree()
+    tree.add('D')
+    tree.add('B')
+    tree.add('A')
+    tree.add('C')
+    tree.add('F')
+    tree.add('E')
+
+    assert tree.preorder() == ['D', 'B', 'A', 'C', 'F', 'E']
+
+
+def test_preorder():
+    tree = BinarySearchTree()
+
+    assert tree.preorder() == []
+
+
+def test_inorder():
+    tree = BinarySearchTree()
+
+    assert tree.inorder() == []
+
+
+def test_inorder():
+    tree = BinarySearchTree()
+    tree.add('D')
+    tree.add('B')
+    tree.add('A')
+    tree.add('C')
+    tree.add('F')
+    tree.add('E')
+
+    assert tree.inorder() == ['A', 'B', 'C', 'D', 'E', 'F']
+
+
+def test_postorder():
+    tree = BinarySearchTree()
+
+    assert tree.postorder() == []
+
+
+def test_postorder():
+    tree = BinarySearchTree()
+    tree.add('D')
+    tree.add('B')
+    tree.add('A')
+    tree.add('C')
+    tree.add('F')
+    tree.add('E')
+
+    assert tree.postorder() == ['A', 'C', 'B', 'E', 'F', 'D']
--- code-challenges/python401/tree/tree.py
+++ code-challenges/python401/tree/tree.py
@@ -12,14 +12,65 @@ class BinaryTree():
     def __init__(self):
         self.root = None
 
-    def preorder(self, root):
-        pass
+    def preorder(self, current=None):
 
-    def inorder(self, root):
-        pass
+        pre_order_list = []
 
-    def postorder(self, root):
-        pass
+        if not self.root:
+            return pre_order_list
+
+        if not current:
+            current = self.root
+
+        pre_order_list.append(current.value)
+
+        if current.left_child is not None:
+            pre_order_list += self.preorder(current.left_child)
+
+        if current.right_child is not None:
+            pre_order_list += self.preorder(current.right_child)
+
+        return pre_order_list
+
+    def inorder(self, current=None):
+
+        in_order_list = []
+
+        if not self.root:
+            return in_order_list
+
+        if not current:
+            current = self.root
+
+        if current.left_child is not None:
+            in_order_list += self.inorder(current.left_child)
+
+        in_order_list.append(current.value)
+
+        if current.right_child is not None:
+            in_order_list += self.inorder(current.right_child)
+
+        return in_order_list
+
+    def postorder(self, current=None):
+
+        post_order_list = []
+
+        if not self.root:
+            return post_order_list
+
+        if not current:
+            current = self.root
+
+        if current.left_child is not None:
+            post_order_list += self.postorder(current.left_child)
+
+        if current.right_child is not None:
+            post_order_list += self.postorder(current.right_child)
+
+        post_order_list.append(current.value)
+
+        return post_order_list
 
 
 class BinarySearchTree(BinaryTree):
@@ -50,17 +101,18 @@ class BinarySearchTree(BinaryTree):
 
     def _contains(self, value):
 
+        current = self.root
+
         if self.root is None:
             return False
-        else:
-            current = self.root
 
-        while current.left_child is not None and current.right_child is not None:
-            if value < current.value:
+        while value != current.value:
+            if value < current.value and current.left_child:
                 current = current.left_child
-            elif value > current.value:
+            elif value > current.value and current.right_child:
                 current = current.right_child
             else:
-                return True
-        return False
-        
+                return False
+
+        return True
+
