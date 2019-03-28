f6497b8396e34407599815be67f24710ec2ac62e
EvyHaan <evy.haan@gmail.com>
2019-03-28 09:52:16 -0700

    commented out code - refactor considerations on left join

--- code-challenges/python401/left_join_hashtable/left_join.py
+++ code-challenges/python401/left_join_hashtable/left_join.py
@@ -9,9 +9,9 @@ def left_join(hashtable1, hashtable2):
             current = hashtable1.table[i].head
             while current:
                 search_key = current.value
-                if hashtable2.contains(search_key[0]):
-                    lst.append([search_key[0], search_key[1], hashtable2.get(search_key[0])])
-                else:
-                    lst.append([search_key[0], search_key[1], None])
+                # if hashtable2.contains(search_key[0]):
+                lst.append([search_key[0], search_key[1], hashtable2.get(search_key[0])])
+                # else:
+                #     lst.append([search_key[0], search_key[1], None])
                 current = current._next
     return lst
