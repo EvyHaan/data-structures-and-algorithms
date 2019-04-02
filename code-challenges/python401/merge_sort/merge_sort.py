f4e46aafb63ab4e91776c9e3fe6a47e5b44d8a3c
EvyHaan <evy.haan@gmail.com>
2019-04-01 22:48:19 -0700

    merge function working

--- code-challenges/python401/merge_sort/merge_sort.py
+++ code-challenges/python401/merge_sort/merge_sort.py
@@ -1,26 +1,26 @@
 def merge_sort(lst):
     lst_len = len(lst)
 
+    if lst_len <= 1:
+        return lst
+
     if lst_len > 1:
-        left_lst, right_lst = merge_sort(lst[0:lst_len//2])
+        left_lst = merge_sort(lst[0:lst_len//2])
         right_lst = merge_sort(lst[lst_len//2:])
 
         sorted_lst = []
-        i = 0
-        j = 0
 
         while left_lst and right_lst:
-            if left_lst[i] < right_lst[j]:
-                sorted_lst.append(left_lst[i])
-                i += 1
+            if left_lst[0] < right_lst[0]:
+                sorted_lst.append(left_lst.pop(0))
+
             else:
-                sorted_lst.append(right_lst[j])
-                j += 1
+                sorted_lst.append(right_lst.pop(0))
 
         if left_lst:
-            sorted_lst.append(left_lst[i:])
+            sorted_lst += left_lst
 
         if right_lst:
-            sorted_lst.append(right_lst[j:])
+            sorted_lst += right_lst
 
         return sorted_lst
