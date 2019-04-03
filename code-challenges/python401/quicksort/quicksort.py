fd40e625f31a0c20b47dfea8836f6ac4684b6d18
EvyHaan <evy.haan@gmail.com>
2019-04-02 20:35:29 -0700

    fixed quicksort

--- code-challenges/python401/quicksort/quicksort.py
+++ code-challenges/python401/quicksort/quicksort.py
@@ -1,5 +1,5 @@
 def quicksort(lst):
-
+    print('sorting list:', lst)
     if len(lst) <= 1:
         return lst
 
@@ -11,17 +11,21 @@ def quicksort(lst):
         while lst[left] < lst[pivot] and left != right:
             left += 1
 
-        while lst[right] < lst[pivot] and right != left:
+        while lst[right] > lst[pivot] and right != left:
             right -= 1
 
         if lst[left] >= lst[pivot] and lst[right] <= lst[pivot]:
             lst[left], lst[right] = lst[right], lst[left]
 
-        if left == right:
-            if lst[left] > lst[pivot]:
-                lst[left], lst[pivot] = lst[pivot], lst[left]
+        if lst[left] > lst[pivot]:
+            lst[left], lst[pivot] = lst[pivot], lst[left]
+            pivot = left
 
-        left_lst = quicksort(lst[:left])
-        right_lst = quicksort(lst[(left + 1):])
+        left_lst = quicksort(lst[:pivot])
+        right_lst = quicksort(lst[pivot + 1:])
 
-    return lst
+        return left_lst + [lst[pivot]] + right_lst
+
+if __name__ == "__main__":
+    test_lst = [3, 2, 4, 5, 1, 6]
+    quicksort(test_lst)
--- code-challenges/python401/quicksort/test_quicksort.py
+++ code-challenges/python401/quicksort/test_quicksort.py
@@ -3,44 +3,44 @@ from quicksort import quicksort
 
 def test_randomly_unsorted_list():
     """An unsorted list returns sorted"""
-    lst = [3, 2, 4, 5, 1]
+    lst = [3, 2, 4, 5, 1, 6]
 
-    expected = [1, 2, 3, 4, 5]
+    expected = [1, 2, 3, 4, 5, 6]
     actual = quicksort(lst)
     assert actual == expected
 
 
-# def test_sorted_list():
-#     """A sorted list will return unchanged"""
-#     lst = ['A', 'B', 'C', 'D', 'E']
+def test_sorted_list():
+    """A sorted list will return unchanged"""
+    lst = ['A', 'B', 'C', 'D', 'E']
 
-#     expected = ['A', 'B', 'C', 'D', 'E']
-#     actual = quicksort(lst)
-#     assert actual == expected
-
-
-# def test_backward_list():
-#     """A backward list will return reversed"""
-#     lst = ['E', 'D', 'C', 'B', 'A']
-
-#     expected = ['A', 'B', 'C', 'D', 'E']
-#     actual = quicksort(lst)
-#     assert actual == expected
+    expected = ['A', 'B', 'C', 'D', 'E']
+    actual = quicksort(lst)
+    assert actual == expected
 
 
-# def test_empty_list():
-#     """An empty list will return unchanged"""
-#     lst = []
+def test_backward_list():
+    """A backward list will return reversed"""
+    lst = ['E', 'D', 'C', 'B', 'A']
 
-#     expected = []
-#     actual = quicksort(lst)
-#     assert actual == expected
+    expected = ['A', 'B', 'C', 'D', 'E']
+    actual = quicksort(lst)
+    assert actual == expected
 
 
-# def test_single_item_list():
-#     """A one-itemed list will return unchanged"""
-#     lst = ['A']
+def test_empty_list():
+    """An empty list will return unchanged"""
+    lst = []
 
-#     expected = ['A']
-#     actual = quicksort(lst)
-#     assert actual == expected
+    expected = []
+    actual = quicksort(lst)
+    assert actual == expected
+
+
+def test_single_item_list():
+    """A one-itemed list will return unchanged"""
+    lst = ['A']
+
+    expected = ['A']
+    actual = quicksort(lst)
+    assert actual == expected
