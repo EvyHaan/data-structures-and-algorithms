c0ef3f6e3e78fdf02706fda83e33ea71f0017ef3
EvyHaan <evy.haan@gmail.com>
2019-04-05 15:20:21 -0700

    restrucutre vertix object

--- code-challenges/python401/graphs/graphs.py
+++ code-challenges/python401/graphs/graphs.py
@@ -1,18 +1,17 @@
 class Graph():
-    """"""
+    """The graph class instantiates a graph."""
 
     def __init__(self):
         self._table = {}
 
     def add_vertex(self, value):
 
-        vertex = Vertex(value)
+        vertex = value
+        self._table[vertex] = {}
+        return vertex
 
-        self._table[value] = []
-        return Vertex
-
-    def add_edge(self, weight=None):
-        pass
+    def add_edge(self, vertex_a, vertex_b, weight=None):
+        
 
     def __repr__(self):
         # dictionary where the key is the value, and the value is a list of it's neighbors values.
@@ -22,14 +21,14 @@ class Graph():
         pass
 
 
-class Vertex():
-    """The vertex class instantiates a node of a graph."""
+# class Vertex():
+#     """The vertex class instantiates a node of a graph."""
 
-    def __init__(self, value):
-        self.value = value
+#     def __init__(self, value):
+#         self.value = {'value': value}
 
-    def __repr__(self):
-        pass
+#     def __repr__(self):
+#         pass
 
-    def __str__(self):
-        pass
+#     def __str__(self):
+#         pass
--- code-challenges/python401/graphs/test_graphs.py
+++ code-challenges/python401/graphs/test_graphs.py
@@ -1,13 +1,13 @@
-from .graphs import Graph, Vertex
+from .graphs import Graph
 
 
-def test_vertex_exists():
-    assert Vertex
+# def test_vertex_exists():
+#     assert Vertex
 
 
-def test_vertex_instance():
-    value = 4
-    assert Vertex(value)
+# def test_vertex_instance():
+#     value = 4
+#     assert Vertex(value)
 
 
 def test_graph_exists():
@@ -22,52 +22,51 @@ def test_add_one_vertex():
     g = Graph()
     vertex = g.add_vertex('apple')
     assert vertex
-    assert vertex.value is 'apple'
     assert 'apple' in g._table
 
 
 def test_add_two_vertex():
-    g = Graph
-    vertex = g.add_vertex('apple')
-    vertex = g.add_vertex('banana')
+    g = Graph()
+    appple = g.add_vertex('apple')
+    banana = g.add_vertex('banana')
     assert 'apple' in g._table
     assert 'banana' in g._table
 
 
-def test_one_edge():
-    g = Graph
-    apple = g.add_vertex('apple')
-    banana = g.add_vertex('banana')
+# def test_one_edge():
+#     g = Graph()
+#     apple = g.add_vertex('apple')
+#     banana = g.add_vertex('banana')
 
-    g.add_edge(apple, banana, 4)
+#     g.add_edge(apple, banana, 4)
 
-    assert g._table['apple'] == '???'
-    assert g._table['banana'] == '???'
-
-
-def test_get_vertices():
-    g = Graph()
-    apple = g.add_vertex('apple')
-    banana = g.add_vertex('banana')
-
-    g.get_vertices()
-
-    assert vertices == '???'
+#     assert g._table['apple'] == '???'
+#     assert g._table['banana'] == '???'
 
 
-def test_get_neightbors():
-    g = Graph()
-    apple = g.add_vertex('apple')
-    banana = g.add_vertex('banana')
+# def test_get_vertices():
+#     g = Graph()
+#     apple = g.add_vertex('apple')
+#     banana = g.add_vertex('banana')
 
-    g.get_neighbors()
+#     g.get_vertices()
 
-    assert neighbors == '???'
+#     assert vertices == '???'
 
 
-def test_graph_size():
-    g = Graph()
-    apple = g.add_vertex('apple')
-    banana = g.add_vertex('banana')
+# def test_get_neightbors():
+#     g = Graph()
+#     apple = g.add_vertex('apple')
+#     banana = g.add_vertex('banana')
 
-    assert len(g) == 2
+#     g.get_neighbors()
+
+#     assert neighbors == '???'
+
+
+# def test_graph_size():
+#     g = Graph()
+#     apple = g.add_vertex('apple')
+#     banana = g.add_vertex('banana')
+
+#     assert len(g._table) == 2
