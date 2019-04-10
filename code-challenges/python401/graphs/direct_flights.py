9f37c37b525d51fa1447e3624adf745b039cf0bd
EvyHaan <evy.haan@gmail.com>
2019-04-09 21:19:51 -0700

    docstrings

--- code-challenges/python401/graphs/direct_flights.py
+++ code-challenges/python401/graphs/direct_flights.py
@@ -3,6 +3,16 @@ from ..stacks_and_queues.stacks_and_queues import Queue
 
 
 def final_destination(graph, lst):
+    """Determines if all stops in an itenerary are possible through direct flights.
+
+    Args:
+        graph: a graph of locations to be searched.
+        lst (lst): a list of cities in an itinerary.
+
+    Returns:
+        'True, <cost>' (str): If the cities are connected by successive direct flights, a string returns 'True' and the total cost of connecting flights.
+        'False, 0' (str): If the cities are not connected by successive direct flights, a string returns 'False' and implies a cost of $0.
+    """
     total_cost = 0
     itinerary = Queue()
 
@@ -17,5 +27,5 @@ def final_destination(graph, lst):
                 total_cost += neighbor[i]
                 break
             else:
-                return 'False, 0'
-        return 'True, {}'.format(total_cost)
+                return 'False, $0'
+        return 'True, ${}'.format(total_cost)
